2865584ab6a92501487d334196eddadce95f0224
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-17 06:17:14 +0000

    Add get question function

--- quiz_interface.py
+++ quiz_interface.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from brain import QuizBrain
 
 BACKGROUND_COLOR = "#009999"
 FONT = ("courier", 20, "bold")
@@ -7,7 +8,8 @@ PADDING = 10
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
         self.interface = Tk()
         self.interface.title("A3AJAGBE QUIZZY APP")
         self.interface.config(padx=20, pady=20, bg=BACKGROUND_COLOR)
@@ -16,7 +18,8 @@ class QuizInterface:
         self.score.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250)
-        self.q_text = self.canvas.create_text(150, 125, text="Question goes here", font=("Arial", 20, "italic"), fill="#008080")
+        self.q_text = self.canvas.create_text(150, 125, width= 280,
+                                              text="Question goes here", font=("Arial", 20, "italic"), fill="#008080")
         self.canvas.grid(row=1, column=0, columnspan=2, pady=30)
 
         self.true = Button(text="True", fg="green")
@@ -27,4 +30,10 @@ class QuizInterface:
         self.false.config(padx=PADDING, pady=PADDING, font=FONT, highlightthickness=0)
         self.false.grid(row=2, column=1)
 
+        self.get_question()
+
         self.interface.mainloop()
+
+    def get_question(self):
+        question = self.quiz.generate_question()
+        self.canvas.itemconfig(self.q_text, text=question)
