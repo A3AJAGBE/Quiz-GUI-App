f03fcc3eced4ed109595ed0488d5bafc1bdd7ae7
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-17 07:24:45 +0000

    Prevent out of range error and disable button

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from data import question_data
 from brain import QuizBrain
 from quiz_interface import QuizInterface
 
-# Default displays
-print('Type "True" or "False" to answer the following questions.')
-
 # Create Question Instance
 question_info = []
 for info in question_data:
@@ -14,15 +11,7 @@ for info in question_data:
     a = info['correct_answer']
     question_info.append(Question(q, a))
 
-# Quiz brain Instance
+# Instances
 quiz_brain = QuizBrain(question_info)
-
-# Quiz Interface Instance
 quiz_interface = QuizInterface(quiz_brain)
 
-# # Iterate through the questions
-# while quiz_brain.next_question():
-#     quiz_brain.generate_question()
-
-print('Completed the quiz successfully.')
-print(f'Final score = {quiz_brain.score}/{quiz_brain.num}')
