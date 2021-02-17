f767d55640281e73de3ace3db4c70e5bf0df4611
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-17 05:28:44 +0000

    Setup the default ui

---
+++ quiz_interface.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+BACKGROUND_COLOR = "#009999"
+
+
+class QuizInterface:
+
+    def __init__(self):
+        self.interface = Tk()
+        self.interface.title("A3AJAGBE QUIZZY APP")
+        self.interface.mainloop()
